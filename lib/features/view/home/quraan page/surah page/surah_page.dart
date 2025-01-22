85056deae6143ea0125e4fecdf697cc9022fd367
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-01-22 16:19:28 +0100

    Refactor SurahPage to improve widget structure; extract ayah display logic into a separate method and enhance card layout for better readability and maintainability.

--- lib/features/view/home/quraan page/surah page/surah_page.dart
+++ lib/features/view/home/quraan page/surah page/surah_page.dart
@@ -43,80 +43,7 @@ class SurahPage extends StatelessWidget {
           const SizedBox(
             height: 10,
           ),
-          Card(
-            elevation: 10,
-            color: Colors.transparent,
-            child: SizedBox(
-              height: Sizeconfig.screenheight! / 2.2,
-              width: Sizeconfig.screenwidth! / 1.12,
-              child: Stack(
-                children: [
-                  Positioned(
-                    top: Sizeconfig.screenheight! / 35,
-                    left: Sizeconfig.screenwidth! / 5,
-                    right: -80,
-                    bottom: -50,
-                    child: ClipRRect(
-                      borderRadius: const BorderRadius.all(Radius.circular(20)),
-                      child: Image.asset(
-                        "lib/core/assets/images/background_image/6221013cR.png",
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: Sizeconfig.screenheight! / 2.2,
-                    width: Sizeconfig.screenwidth! / 1.12,
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(Radius.circular(20)),
-                      color: kmaincolor4.withOpacity(0.6),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        _cardtext(surah.name, 29),
-                        _cardtext(
-                            '${surah.revelationType.tr}, ${surah.ayahs.length} ${"verses".tr}',
-                            20),
-                        const Divider(
-                          endIndent: 40,
-                          indent: 40,
-                          thickness: 1,
-                          color: Colors.white,
-                        ),
-                        SizedBox(
-                          height: Sizeconfig.screenheight! / 8,
-                          width: Sizeconfig.screenwidth! / 1.2,
-                          child: Image.asset(
-                            "lib/core/assets/images/background_image/isti3ada.png",
-                            fit: BoxFit.contain,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Visibility(
-                          visible: surah.number == 9 || surah.number == 1
-                              ? false
-                              : true,
-                          child: SizedBox(
-                            height: Sizeconfig.screenheight! / 8,
-                            width: Sizeconfig.screenwidth! / 1.2,
-                            child: Image.asset(
-                              "lib/core/assets/images/background_image/basmala.png",
-                              fit: BoxFit.contain,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+          _surahCard(),
           SizedBox(
             height: Sizeconfig.screenheight! / 20,
           ),
@@ -125,77 +52,154 @@ class SurahPage extends StatelessWidget {
             shrinkWrap: true,
             itemCount: surah.ayahs.length,
             itemBuilder: (context, index) {
-              return Column(
-                children: [
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(Radius.circular(10)),
-                      color: kmaincolor4.withOpacity(0.1),
-                    ),
-                    height: 60,
-                    width: Sizeconfig.screenwidth! / 1.16,
-                    child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          CircleAvatar(
-                            radius: 22,
-                            backgroundColor: kmaincolor4,
-                            child: Text(
-                              "{${surah.ayahs[index].numberInSurah}}",
-                              style: const TextStyle(color: Colors.white),
-                            ),
-                          ),
-                          Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              _iconbuttons(
-                                  Icons.play_circle_outline_rounded, () {}),
-                              const SizedBox(
-                                width: 10,
-                              ),
-                              _iconbuttons(
-                                  Icons.bookmark_outline_rounded, () {}),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  SizedBox(
-                    width: Sizeconfig.screenwidth! / 1.16,
-                    child: Text(
-                      verses[index].text,
-                      style: const TextStyle(
-                          fontFamily: 'UthmanicHafs', fontSize: 34),
-                      textAlign: TextAlign.start,
-                      textDirection: TextDirection.rtl,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  const Divider(
-                    endIndent: 30,
-                    indent: 30,
-                    thickness: 1,
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                ],
-              );
+              return _ayahWidget(index);
             },
           ),
         ],
       )),
+    );
+  }
+
+  Column _ayahWidget(int index) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 10,
+        ),
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+            color: kmaincolor4.withOpacity(0.1),
+          ),
+          height: 60,
+          width: Sizeconfig.screenwidth! / 1.16,
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CircleAvatar(
+                  radius: 22,
+                  backgroundColor: kmaincolor4,
+                  child: Text(
+                    "{${surah.ayahs[index].numberInSurah}}",
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    _iconbuttons(Icons.play_circle_outline_rounded, () {}),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    _iconbuttons(Icons.bookmark_outline_rounded, () {}),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        SizedBox(
+          width: Sizeconfig.screenwidth! / 1.16,
+          child: Text(
+            verses[index].text,
+            style: const TextStyle(fontFamily: 'UthmanicHafs', fontSize: 34),
+            textAlign: TextAlign.start,
+            textDirection: TextDirection.rtl,
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        const Divider(
+          endIndent: 30,
+          indent: 30,
+          thickness: 1,
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+      ],
+    );
+  }
+
+  Card _surahCard() {
+    return Card(
+      elevation: 10,
+      color: Colors.transparent,
+      child: SizedBox(
+        height: Sizeconfig.screenheight! / 2.2,
+        width: Sizeconfig.screenwidth! / 1.12,
+        child: Stack(
+          children: [
+            Positioned(
+              top: Sizeconfig.screenheight! / 35,
+              left: Sizeconfig.screenwidth! / 5,
+              right: -80,
+              bottom: -50,
+              child: ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(20)),
+                child: Image.asset(
+                  "lib/core/assets/images/background_image/6221013cR.png",
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            Container(
+              height: Sizeconfig.screenheight! / 2.2,
+              width: Sizeconfig.screenwidth! / 1.12,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(20)),
+                color: kmaincolor4.withOpacity(0.6),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  _cardtext(surah.name, 29),
+                  _cardtext(
+                      '${surah.revelationType.tr}, ${surah.ayahs.length} ${"verses".tr}',
+                      20),
+                  const Divider(
+                    endIndent: 40,
+                    indent: 40,
+                    thickness: 1,
+                    color: Colors.white,
+                  ),
+                  SizedBox(
+                    height: Sizeconfig.screenheight! / 8,
+                    width: Sizeconfig.screenwidth! / 1.2,
+                    child: Image.asset(
+                      "lib/core/assets/images/background_image/isti3ada.png",
+                      fit: BoxFit.contain,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Visibility(
+                    visible:
+                        surah.number == 9 || surah.number == 1 ? false : true,
+                    child: SizedBox(
+                      height: Sizeconfig.screenheight! / 8,
+                      width: Sizeconfig.screenwidth! / 1.2,
+                      child: Image.asset(
+                        "lib/core/assets/images/background_image/basmala.png",
+                        fit: BoxFit.contain,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
