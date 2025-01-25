ed629ecfcea3122b668875ea26988f1d267698fc
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-01-25 21:40:59 +0100

    Simplify AyahSearch title by removing unnecessary padding in SearchBar widget

--- lib/features/view/home/quraan page/ayah search/ayah_search.dart
+++ lib/features/view/home/quraan page/ayah search/ayah_search.dart
@@ -8,12 +8,9 @@ class AyahSearch extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: SearchBar(
-            elevation: WidgetStatePropertyAll(0),
-            backgroundColor: WidgetStatePropertyAll(Colors.white),
-          ),
+        title: const SearchBar(
+          elevation: WidgetStatePropertyAll(0),
+          backgroundColor: WidgetStatePropertyAll(Colors.white),
         ),
       ),
       body: Container(),
